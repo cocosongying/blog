879aadca86c4bdc80670a6ad8a3235eb875ec129
宋颖 <cocosongying@gmail.com>
2017-12-28 12:44:30 +0800

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,5 +1,5 @@
 var article_list = new Array();
 var blogcnt = 0;
-article_list[blogcnt++] = '### 📜 [Markdown 语法简介](index.html?file=markdown)\r\n📅 `2017-12-27 22:11` 🏷️ `markdown语法`\r\n```\r\nMarkdown是一种可以使用普通文本编辑器编写的标记语言，通过简单的标记语法，它可以使普通文本内容具有一定的格式。\r\n这里简单介绍Markdown语言常用语法。\r\n```\r\n';
-article_list[blogcnt++] = '### 📜 [Emoji表情符号大全](index.html?file=emoji)\r\n📅 `2017-12-27 22:32` 🏷️ `emoji` 🏷️ `表情`\r\n```\r\nEmoji是在无线通信中所使用的视觉情感符号，可用来代表多种表情，如笑脸表示笑、蛋糕表示食物等。\r\n这里从emojipedia.org和getemoji.com中收集了一些表情符号。\r\n```\r\n';
-// article_list[blogcnt] = '### 📜 [标题](index.html?file=Markdown)\r\n📅 `2018-01-01 00:00` 🏷️ `关键词` 🏷️ `关键词`\r\n```\r\n摘要\r\n摘要\r\n```\r\n';
+article_list[blogcnt++] = '### 📜 [Markdown 语法简介](index.html?file=markdown)\r\n📅 `2017-12-27 22:11` 🔖 `markdown语法`\r\n```\r\nMarkdown是一种可以使用普通文本编辑器编写的标记语言，通过简单的标记语法，它可以使普通文本内容具有一定的格式。\r\n这里简单介绍Markdown语言常用语法。\r\n```\r\n';
+article_list[blogcnt++] = '### 📜 [Emoji表情符号大全](index.html?file=emoji)\r\n📅 `2017-12-27 22:32` 🔖 `emoji` 🔖 `表情`\r\n```\r\nEmoji是在无线通信中所使用的视觉情感符号，可用来代表多种表情，如笑脸表示笑、蛋糕表示食物等。\r\n这里从emojipedia.org和getemoji.com中收集了一些表情符号。\r\n```\r\n';
+// article_list[blogcnt] = '### 📜 [标题](index.html?file=Markdown)\r\n📅 `2018-01-01 00:00` 🔖 `关键词` 🔖 `关键词`\r\n```\r\n摘要\r\n摘要\r\n```\r\n';
