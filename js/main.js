af3bb7f1fcd83a8a256b026214b4f62e6414c075
宋颖 <cocosongying@gmail.com>
2017-12-28 13:23:32 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -64,6 +64,64 @@ function showBlogList(jsondata) {
     });
 }
 
+var imgUrl = 'icon.ico';  // 分享后展示的一张图片
+var lineLink = window.location.href; // 点击分享后跳转的页面地址
+var descContent = "磨叽熊猫博客吃饭睡觉打豆豆磨叽磨叽磨叽。。。";  // 分享后的描述信息
+var shareTitle = '磨叽熊猫🐼';  // 分享后的标题
+var appid = '';  // 应用id,如果有可以填，没有就留空
+
+function shareFriend() {
+    WeixinJSBridge.invoke('sendAppMessage',{
+        "appid": appid,
+        "img_url": imgUrl,
+        "img_width": "200",
+        "img_height": "200",
+        "link": lineLink,
+        "desc": descContent,
+        "title": shareTitle
+    }, function(res) {
+        //_report('send_msg', res.err_msg);  // 这是回调函数，必须注释掉
+    })
+}
+
+function shareTimeline() {
+    WeixinJSBridge.invoke('shareTimeline',{
+        "img_url": imgUrl,
+        "img_width": "200",
+        "img_height": "200",
+        "link": lineLink,
+        "desc": descContent,
+        "title": shareTitle
+    }, function(res) {
+        //_report('timeline', res.err_msg); // 这是回调函数，必须注释掉
+    });
+}
+
+function shareWeibo() {
+    WeixinJSBridge.invoke('shareWeibo',{
+        "content": descContent,
+        "url": lineLink,
+    }, function(res) {
+        //_report('weibo', res.err_msg);
+    });
+}
+
+// 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
+document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+    // 发送给好友
+    WeixinJSBridge.on('menu:share:appmessage', function(argv){
+        shareFriend();
+    });
+    // 分享到朋友
+    WeixinJSBridge.on('menu:share:timeline', function(argv){
+        shareTimeline();
+    });
+    // 分享到微博
+    WeixinJSBridge.on('menu:share:weibo', function(argv){
+        shareWeibo();
+    });
+}, false);
+
 /* Judging the content of the display according to the parameters in the URL */
 var file=getQueryString("file");
 var search=getQueryString("search");
